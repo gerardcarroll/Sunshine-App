feff41be8c35e70475417a92aad87626bf1ed036
Gerard Carroll <gcarroll@overstock.com>
2016-02-15 16:39:57 +0000

    end of stage 1

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,27 +1,25 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2014 The Android Open Source Project Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,64 +27,97 @@ import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
+  private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction().add(R.id.container, new DetailFragment()).commit();
+    }
+  }
+
+  @Override
+  @TargetApi(14)
+  public boolean onCreateOptionsMenu(final Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.detail, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(final MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    final int id = item.getItemId();
+
+    // noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      startActivity(new Intent(this, SettingsActivity.class));
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * A placeholder fragment containing a simple view.
+   */
+  public static class DetailFragment extends Fragment {
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+    private static String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+
+    private String forecastString;
+
+    public DetailFragment() {
+      setHasOptionsMenu(true);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailFragment())
-                    .commit();
-        }
-    }
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+      final Bundle savedInstanceState) {
 
+      final View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
+      // The detail Activity called via intent. Inspect the intent for forecast data.
+      final Intent intent = getActivity().getIntent();
+      if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+        forecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
+        ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastString);
+      }
+
+      return rootView;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      inflater.inflate(R.menu.detailfragment, menu);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
+      // Locate MenuItem with ShareActionProvider
+      final MenuItem item = menu.findItem(R.id.menu_item_share);
 
-        return super.onOptionsItemSelected(item);
+      // Fetch and store ShareActionProvider
+      final ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+      if (mShareActionProvider != null) {
+        mShareActionProvider.setShareIntent(createShareIntent());
+      }
+      else {
+        Log.d(LOG_TAG, "Share Action Provider is null?");
+      }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class DetailFragment extends Fragment {
-
-        public DetailFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-
-            // The detail Activity called via intent.  Inspect the intent for forecast data.
-            Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(forecastStr);
-            }
-
-            return rootView;
-        }
+    private Intent createShareIntent() {
+      final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+      shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+      shareIntent.setType("text/plain");
+      shareIntent.putExtra(Intent.EXTRA_TEXT, forecastString + FORECAST_SHARE_HASHTAG);
+      return shareIntent;
     }
+  }
 }
